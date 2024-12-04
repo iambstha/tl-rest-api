be33cd23b0956fb9900dedd29c1c745b358f6d0d
Bishal Shrestha <iambstha@gmail.com>
2024-12-04 10:44:24 +0545

    initial commit with base setup

---
+++ src/main/java/com/iambstha/tl_rest_api/TlRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.iambstha.tl_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iambstha/tl_rest_api/TlRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iambstha.tl_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
