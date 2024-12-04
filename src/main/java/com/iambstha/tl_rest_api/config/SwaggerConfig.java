efe00349136c780fc1af527f003b99d972074dab
Bishal Shrestha <iambstha@gmail.com>
2024-12-04 11:43:02 +0545

    validator and swagger configuration added

---
+++ src/main/java/com/iambstha/tl_rest_api/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.iambstha.tl_rest_api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info().title("Tree Leaf API")
+                        .description("Application API for Tree Leaf services.")
+                        .version("v0.0.1")
+                        .contact(new Contact()
+                                .name("Bishal Shrestha")
+                                .email("iambstha@gmail.com")
+                                .url("https://bishal-shrestha.com.np")
+                        )
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")
+                        )
+                        .termsOfService("/v1/api/public/tos")
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new
+                                io.swagger.v3.oas.models.security.SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
