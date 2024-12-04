dfbcccee8f6e358a68787c9be1f22c1385157bb7
Bishal Shrestha <iambstha@gmail.com>
2024-12-04 12:39:38 +0545

    refresh token services added

--- src/main/java/com/iambstha/tl_rest_api/exception/AuthException.java
+++ src/main/java/com/iambstha/tl_rest_api/exception/AuthException.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.io.Serial;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class DpkAuthException extends RuntimeException {
+public class AuthException extends RuntimeException {
     /**
      *
      */
     @Serial
     private static final long serialVersionUID = 3909083199557817547L;
 
-    public DpkAuthException(String message) {
+    public AuthException(String message) {
         super(message);
     }
 }
